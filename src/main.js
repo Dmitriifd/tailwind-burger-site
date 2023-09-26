676e0bc950470d7a2ec77e1fb270c82e98d5644f
Dmitriifd <idimka9@gmail.com>
2023-09-26 21:15:45 +0500

    menu tabs section

--- src/main.js
+++ src/main.js
@@ -17,3 +17,44 @@ closeIcon.addEventListener('click', () => {
 hamburger.addEventListener('click', () => {
   navMenu.classList.remove('hidden');
 });
+
+/* TABS */
+const tabs = document.querySelectorAll('.tabs_wrap ul li');
+const allItems = document.querySelectorAll('.item_wrap');
+const foods = document.querySelectorAll('.food');
+const beverages = document.querySelectorAll('.beverage');
+const snacks = document.querySelectorAll('.snack');
+
+tabs.forEach((tab) => {
+  tab.addEventListener('click', () => {
+    tabs.forEach((tab) => {
+      tab.classList.remove('active');
+    });
+
+    tab.classList.add('active');
+
+    const tabval = tab.getAttribute('data-tabs');
+
+    allItems.forEach((item) => {
+      item.style.display = 'none';
+    });
+
+    if (tabval == 'food') {
+      foods.forEach((item) => {
+        item.style.display = 'block';
+      });
+    } else if (tabval == 'snack') {
+      snacks.forEach((item) => {
+        item.style.display = 'block';
+      });
+    } else if (tabval == 'beverage') {
+      beverages.forEach((item) => {
+        item.style.display = 'block';
+      });
+    } else {
+      allItems.forEach((item) => {
+        item.style.display = 'block';
+      });
+    }
+  });
+});
